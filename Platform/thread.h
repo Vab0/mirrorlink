934bf8fcfb754e647ac4db65c088a25cde448628
wusunjie <wusunjie123@hotmail.com>
2015-09-12 07:13:57 -0700

    fix a write mistake.

--- Platform/thread.h
+++ Platform/thread.h
@@ -7,7 +7,7 @@ typedef void (*thread_func)(void);
 
 typedef void (*timer_func)(int fd, void *args);
 
-extern void thread_post(thread_func f, void *args)
+extern void thread_post(thread_func f, void *args);
 
 extern int timer_start(uint32_t s, timer_func f, void *args);
 
